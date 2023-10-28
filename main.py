4a734072f1c88985994cd73c4d015bf32f4e1c4f
suphakin-th <suphakin.th@gmail.com>
2023-10-29 03:23:12 +0700

    Finish qr payment for subc feature.

---
+++ main.py
@@ -0,0 +1,37 @@
+# Python3.9.18
+# Overwrite from old component
+
+import os
+
+from datetime import datetime
+from pypromptpay import qr_code
+
+
+# Set global path
+## EX. os.path.join(BASE_DIR, 'next_folder/')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_PATH = os.path.join(BASE_DIR, 'THAI_QRCODE/')
+
+def generate_qr_code(account:str, currency:str, money:str):
+    # Check if the directory 'QRCODE' exists; create it if not
+    qr_code_dir = os.path.join(PROJECT_PATH, 'QRCODE/')
+    print(qr_code_dir)
+    if not os.path.exists(qr_code_dir):
+        os.makedirs(name=qr_code_dir)
+
+    # Generate QRCode
+    qr_code(
+        account=account,
+        one_time=True, 
+        path_qr_code=os.path.join(qr_code_dir, f"qrcode_{account}_{currency}_{datetime.now().strftime('%Y%m%d%H%M%S')}.PNG"), 
+        country="TH", 
+        money=money, 
+        currency=currency
+    )
+
+if __name__ == "__main__":
+    generate_qr_code(
+        account='1314098002729',
+        currency='THB',
+        money='100'
+    )
---
+++ pypromptpay.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+import crc16
+import json
+import os
+import pytz
+import qrcode
+
+from datetime import datetime
+
+def get_datetimezone_now(currency:str = 'THB'):
+    utc_now = datetime.utcnow()
+    file_path = 'currency_timezone_dict.json'
+    timezone = None
+    
+    # Check if the file exists and is a regular file
+    if os.path.isfile(file_path):
+        try:
+            with open(file_path, 'r') as file:
+                currency_timezone_dict = json.load(file)
+                timezone = currency_timezone_dict.get(currency)
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON in {file_path}: {e}")
+    else:
+        print(f"{file_path} is not a regular file or does not exist.")
+
+    if timezone:
+        # Convert UTC time to the desired timezone
+        return utc_now.replace(tzinfo=pytz.utc).astimezone(pytz.timezone(timezone))
+    else:
+        # Handle the case where the currency is not found in the dictionary
+        print(f"Timezone for currency {currency} not found in {file_path}.")
+        return utc_now.replace(tzinfo=pytz.utc).astimezone(pytz.timezone('Asia/Bangkok'))
+
+def qr_code(account,one_time=True,path_qr_code="",country="TH",money="",currency="THB"):
+    """
+    qr_code(account,one_time=True,path_qr_code="",country="TH",money="",currency="THB")
+    account is phone number or  identification number.
+    one_time : if you use once than it's True.
+    path_qr_code : path save qr code.
+    country : TH
+    money : money (if have)
+    currency : THB
+    """
+    Version = "0002"+"01" # เวชั่นของ  PromptPay
+    if one_time == True: # one_time คือ ต้องการให้โค้ดนี้ครั้งเดียวหรือไม่
+        one_time = "010212" # 12 ใช้ครั้งเดียว
+    else:
+        one_time ="010211" # 11 ใช้ได้้หลายครั้ง
+    
+    if len(account) == 10 or len(account) == 13 : 
+        merchant_account_information = "2937" # ข้อมูลผู้ขาย (เฉพาะเบอร์โทร และ บัตรประชาชน)
+    else :
+        merchant_account_information = "2939" # ข้อมูลผู้ขาย (เฉพาะเลขอ้างอิง)
+        
+    merchant_account_information += "0016"+"A000000677010111" # หมายเลขแอปพลิเคชั่น PromptPay
+    if len(account) == 10: #ถ้าบัญชีนี้เป็นเบอร์โทร
+        account = list(account)
+        merchant_account_information += "011300" # 01 หมายเลขโทรศัพท์ ความยาว 13 ขึ้นต้น 00
+        if country == "TH":
+            merchant_account_information += "66" # รหัสประเทศ 66 คือประเทศไทย
+        del account[0] # ตัดเลข 0 หน้าเบอร์ออก
+        merchant_account_information += ''.join(account)
+    elif len(account) == 13 : #ถ้าบัญชีนี้เป็นบัตรประชาชน
+        merchant_account_information += "0213" + account.replace('-','')
+    else : #ไม่ใช่เบอร์โทร และ บัตรประชาชน เป็นเลขอ้างอิง
+        merchant_account_information += "0315" + account + "5303764"
+    country = "5802" + country # ประเทศ
+    if currency == "THB":
+        currency = "5303" + "764" # "764"  คือเงินบาทไทย ตาม https://en.wikipedia.org/wiki/ISO_4217
+    if money != "": # กรณีกำหนดเงิน
+        check_money = money.split('.') # แยกจาก .
+        if len(check_money) == 1 or len(check_money[1]) == 1: # กรณีที่ไม่มี . หรือ มีทศนิยมแค่หลักเดียว
+            money = "54" + "0" + str(len(str(float(money))) + 1) + str(float(money)) + "0"
+        else:
+            money = "54" + "0" + str(len(str(float(money)))) + str(float(money)) # กรณีที่มีทศนิยมครบ
+    check_sum = Version+one_time+merchant_account_information+country+currency+money+"6304" # เช็คค่า check sum
+    check_sum1 = hex(crc16.crc16xmodem(check_sum.encode('ascii'),0xffff)).replace('0x','')
+    if len(check_sum1) < 4: # # แก้ไขข้อมูล check_sum ไม่ครบ 4 หลัก
+        check_sum1 = ("0"*(4-len(check_sum1))) + check_sum1
+    check_sum += check_sum1
+    if path_qr_code != "":
+        img = qrcode.make(check_sum.upper())
+        imgload = open(path_qr_code,'wb')
+        img.save(imgload, 'PNG')
+        imgload.close()
+        return True
+    else:
+        return check_sum.upper() # upper ใช้คืนค่าสตริงเป็นตัวพิมพ์ใหญ่
